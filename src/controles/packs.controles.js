97236c735b97b34595e8e9bf4fb3f7a31e8b024c
ERIKA9311 <guerreroerika408@gmail.com>
2026-05-25 12:06:36 -0500

    Feat: agregar ruta y controlador para eliminar packs

--- src/controles/packs.controles.js
+++ src/controles/packs.controles.js
@@ -94,6 +94,24 @@ const reservarPack = async (req, res) => {
        
 };
 
+const eliminarPack = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        // Ejecutamos la consulta SQL de eliminación utilizando el ID de la ruta
+        const [resultado] = await pool.query('DELETE FROM packs WHERE id = ?', [id]);
+
+        // Si no afectó ninguna fila, significa que ese ID no existía
+        if (resultado.affectedRows === 0) {
+            return res.status(404).json({ mensaje: 'El pack no existe o ya fue eliminado.' });
+        }
+
+        return res.json({ mensaje: '¡Publicación eliminada con éxito!' });
+    } catch (error) {
+        console.error('Error al eliminar el pack:', error);
+        return res.status(500).json({ mensaje: 'Error interno del servidor al eliminar.' });
+    }
+};
 
 
-module.exports = {crearPack, obtenerPacks, obtenerPacksPorUsuario,reservarPack };
+module.exports = {crearPack, obtenerPacks, obtenerPacksPorUsuario,reservarPack,eliminarPack };
--- src/rutas/packs.rutas.js
+++ src/rutas/packs.rutas.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { crearPack, obtenerPacks, obtenerPacksPorUsuario,reservarPack } = require('../controles/packs.controles');
+const { crearPack, obtenerPacks, obtenerPacksPorUsuario,reservarPack, eliminarPack } = require('../controles/packs.controles');
 
 // Estas rutas se sumarán a /api/packs
 router.get('/', obtenerPacks);    
 router.put('/reservar/:id', reservarPack);               // GET /api/packs
 router.get('/usuario/:usuario_id', obtenerPacksPorUsuario); // GET /api/packs/usuario/:id
-router.post('/', crearPack);                    // POST /api/packs
+router.post('/', crearPack);   
+router.delete('/:id', eliminarPack);                 // POST /api/packs
 
 
 module.exports = router;
